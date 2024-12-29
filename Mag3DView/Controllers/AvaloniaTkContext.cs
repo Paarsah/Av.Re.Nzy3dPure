005a704e712c296fb927da623e47eba6d3d9695d
Paarsah <Paarsah.Soroury@Gmail.com>
2024-12-28 20:02:05 -0800

    Update ...

---
+++ Mag3DView/Controllers/AvaloniaTkContext.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.OpenGL;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mag3DView.Controllers
+{
+    class AvaloniaTkContext : IBindingsContext
+    {
+        private readonly GlInterface _glInterface;
+        public AvaloniaTkContext(GlInterface glInterface) => _glInterface = glInterface;
+        public nint GetProcAddress(string procName) => _glInterface.GetProcAddress(procName);
+    }
+}
